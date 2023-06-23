bafa26dd4f62ee3e42785f67de2b03b8b741abe5
Niklas Birth <birth@posteo.de>
2023-06-23 11:09:21 +0200

    Added requirement numba-progress

--- src/cli.py
+++ src/cli.py
@@ -1,15 +1,19 @@
+import os
 import sys
 
 from cocopye.ui import terminal
 
 
 def main() -> None:
-    terminal.main()
+    try:
+        terminal.main()
+    except KeyboardInterrupt:
+        print("\n\nInterrupted.")
+        try:
+            sys.exit(130)
+        except SystemExit:
+            os._exit(130)
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except KeyboardInterrupt:
-        print("\n\nInterrupted.")
-        sys.exit(130)
+    main()
