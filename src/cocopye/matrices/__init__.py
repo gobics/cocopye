knn fixes, normalization

src/cocopye/matrices/__init__.py
@@ -107,7 +107,7 @@ class DatabaseMatrix(Matrix[npt.NDArray[np.uint8]]):
             vec: npt.NDArray[np.uint8],
             k: int,
             frac_eq: float = 0.9,
-            knn: Optional[DatabaseMatrix] = None,
+            knn: Optional[npt.NDArray[np.uint64]] = None
     ) -> Tuple[float, float, int]:
         """
         Calculate an estimate for completeness and contamination for a vector based on common markers in the k nearest
@@ -116,13 +116,11 @@ class DatabaseMatrix(Matrix[npt.NDArray[np.uint8]]):
         :param k: k (like in k nearest neighbors)
         :param frac_eq: Fraction of similar counts within the nearest neighbors required to consider a Pfam/kmer as a
         marker
-        :param knn: If provided, this is used as the k nearest neighbors
+        :param knn: If provided, this is used as the nearest neighbors (expects a 1D numpy array with database indeices)
         :return: A 3-tuple: First element is the completeness estimate, the second is the contamiation estimate
         (both between 0 and 1) and the third one is the number of markers that were used. This last value is mainly
         intended for evaluation purposes.
         """
-        if knn is not None:
-            knn = knn.mat()
         return estimate_njit(self.mat(), vec, k, frac_eq, knn)
 
 

src/cocopye/matrices/_numba_functions.py
@@ -89,8 +89,9 @@ def nearest_neighbors_idx_njit(mat: npt.NDArray[np.uint8], vec: npt.NDArray[np.u
     assert vec.shape[0] == num_count, "Vector length must be equal to the number of columns of the matrix"
 
     eq_count = np.zeros(num_refs)
+    norm_vec = np.sqrt((mat > 0).sum(axis=1))
     for idx in prange(len(mat)):
-        eq_count[idx] = np.sum(np.logical_and((0 < vec) < 255, mat[idx] == vec))
+        eq_count[idx] = np.sum(np.logical_and(np.logical_and(0 < vec, vec < 255), mat[idx] == vec)) / norm_vec[idx]
 
     return np.flip(np.argsort(eq_count))[:k]
 